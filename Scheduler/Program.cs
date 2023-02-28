ef3caacfe3b2ed779b4e2dae32348cd010bc9d7e
Abbi Hankey <67207650+abbihankey@users.noreply.github.com>
2023-02-28 14:15:15 -0500

    example

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -20,27 +20,106 @@ namespace Scheduler
 {
     static class Program
     {
-        /* try
-    {
-        using(con = new SqlConnection(Properties.Settings.Default.SchoolConnectionString))
-        {
-            con.Open();
-            string sqlCommand = "Update (Table) set value=@Value where id=@ID";
-            SqlCommand cmd = new SqlCommand(sqlCommand, con);
-            cmd.Parameters.AddWithValue("@Value", updatedValue);
-            cmd.Parameters.AddWithValue("@ID", idOfRowToUpdate);
-            int rowsAffected = cmd.ExecuteNonQuery();
-            if(rowsAffected == 1)
-            {
-                MessageBox.Show("Information Updated", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            con.Close();
-        }
-    }
-    catch (Exception ex)
-    {
-        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-    } */
+                /* 
+                        namespace InsertUpdateDeleteDemo  
+        {  
+            public partial class frmMain : Form  
+            {  
+                SqlConnection con= new SqlConnection("Data Source=.;Initial Catalog=Sample;Integrated Security=true;");  
+                SqlCommand cmd;  
+                SqlDataAdapter adapt;  
+                //ID variable used in Updating and Deleting Record  
+                int ID = 0;  
+                public frmMain()  
+                {  
+                    InitializeComponent();  
+                    DisplayData();  
+                }  
+                //Insert Data  
+                private void btn_Insert_Click(object sender, EventArgs e)  
+                {  
+                    if (txt_Name.Text != "" && txt_State.Text != "")  
+                    {  
+                        cmd = new SqlCommand("insert into tbl_Record(Name,State) values(@name,@state)", con);  
+                        con.Open();  
+                        cmd.Parameters.AddWithValue("@name", txt_Name.Text);  
+                        cmd.Parameters.AddWithValue("@state", txt_State.Text);  
+                        cmd.ExecuteNonQuery();  
+                        con.Close();  
+                        MessageBox.Show("Record Inserted Successfully");  
+                        DisplayData();  
+                        ClearData();  
+                    }  
+                    else  
+                    {  
+                        MessageBox.Show("Please Provide Details!");  
+                    }  
+                }  
+                //Display Data in DataGridView  
+                private void DisplayData()  
+                {  
+                    con.Open();  
+                    DataTable dt=new DataTable();  
+                    adapt=new SqlDataAdapter("select * from tbl_Record",con);  
+                    adapt.Fill(dt);  
+                    dataGridView1.DataSource = dt;  
+                    con.Close();  
+                }  
+                //Clear Data  
+                private void ClearData()  
+                {  
+                    txt_Name.Text = "";  
+                    txt_State.Text = "";  
+                    ID = 0;  
+                }  
+                //dataGridView1 RowHeaderMouseClick Event  
+                private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)  
+                {  
+                    ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());  
+                    txt_Name.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();  
+                    txt_State.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();  
+                }  
+                //Update Record  
+                private void btn_Update_Click(object sender, EventArgs e)  
+                {  
+                    if (txt_Name.Text != "" && txt_State.Text != "")  
+                    {  
+                        cmd = new SqlCommand("update tbl_Record set Name=@name,State=@state where ID=@id", con);  
+                        con.Open();  
+                        cmd.Parameters.AddWithValue("@id", ID);  
+                        cmd.Parameters.AddWithValue("@name", txt_Name.Text);  
+                        cmd.Parameters.AddWithValue("@state", txt_State.Text);  
+                        cmd.ExecuteNonQuery();  
+                        MessageBox.Show("Record Updated Successfully");  
+                        con.Close();  
+                        DisplayData();  
+                        ClearData();  
+                    }  
+                    else  
+                    {  
+                        MessageBox.Show("Please Select Record to Update");  
+                    }  
+                }  
+                //Delete Record  
+                private void btn_Delete_Click(object sender, EventArgs e)  
+                {  
+                    if(ID!=0)  
+                    {  
+                        cmd = new SqlCommand("delete tbl_Record where ID=@id",con);  
+                        con.Open();  
+                        cmd.Parameters.AddWithValue("@id",ID);  
+                        cmd.ExecuteNonQuery();  
+                        con.Close();  
+                        MessageBox.Show("Record Deleted Successfully!");  
+                        DisplayData();  
+                        ClearData();  
+                    }  
+                    else  
+                    {  
+                        MessageBox.Show("Please Select Record to Delete");  
+                    }  
+                } 
+                 */
 
 
 
